d2414873d9712619044d0715214cab0fcaafe3b5
timothy banks <timothyaaronbanks@gmail.com>
2024-07-15 21:15:05 -0400

    Current source is in a compilable state.

---
+++ tests/select_context_tests.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE select_context_test
+
+#include <cpplinq/cpplinq.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(select_context)
+{
+    // int x = 4;
+    // int y = 2 + 2;
+    // BOOST_TEST(x == y);
+}
